9d67ad423bc9c17e5ef7fe12434e192daf1bf0df
veethota <veethota@deloitte.com>
2023-07-28 18:08:40 +0530

    Reformat the python code!!

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # W.A.p to print even numbers and odd numbers from the given range of numbers
 
 for i in range(10):
-    if i%2==0:
-        print(i,end=" ")
+    if i % 2 == 0:
+        print(i, end=" ")
